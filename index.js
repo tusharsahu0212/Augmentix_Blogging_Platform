58e83f1a690cc0f842708134564559c634546dfb
tusharsahu0212 <tusharsahu0212@gmail.com>
2023-08-12 17:25:33 +0530

    complete building api 1.0 and starting templates

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ const blogSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true
-      },
+    },
     content: {
         type: String,
         required: true
-      },
+    },
     image: Buffer,
     video: Buffer
-}); 
+});
 
 
 
@@ -37,86 +37,194 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true
-      },
+    },
     password: {
         type: String,
         required: true
-      },
-    blogs: [blogSchema]
+    },
+    blogs: {
+        type: [blogSchema],
+        default: []
+    }
 
-  });
+});
 
 const Blog = mongoose.model('Blog', blogSchema);
 const User = mongoose.model('User', userSchema);
 
 
 const upload = multer({
-    limits:{
-        fileSize: 20*1024*1024
+    limits: {
+        fileSize: 20 * 1024 * 1024
     }
 });
 
 
 const cpUpload = upload.fields([{ name: 'image', maxCount: 1 }, { name: 'video', maxCount: 1 }])
 
-app.get('/home', (req,res)=>{
+app.get('/home', (req, res) => {
     res.send("Home");
 })
 
 
 
 // create
-app.post('/upload', cpUpload, async(req,res, next)=>{
+app.post('/upload', cpUpload, async (req, res) => {
 
-    const title = req.body.title;
-    const content = req.body.content;
-    const imageBuffer = await req.files['image'][0].buffer;
-    const videoBuffer = await req.files['video'][0].buffer;
+    let imageBuffer = null;
+    let videoBuffer = null;
+
+    if (req.body.image) {
+        imageBuffer = await req.files['image'][0].buffer;
+
+    }
+
+    if (req.body.video) {
+        videoBuffer = await req.files['video'][0].buffer;
+
+    }
 
     const newBlog = new Blog({
-        title: title,
-        content: content,
+        title: req.body.title,
+        content: req.body.content,
         image: imageBuffer,
         video: videoBuffer
     })
 
     await newBlog.save();
 
-    res.status(200).send(req.body);
+    User.findOne({ username: req.body.username }).then((data, err) => {
+
+        if (err) {
+            console.log(err);
+        } else {
+            data.blogs.push(newBlog);
+        }
+        res.status(200).send(data);
+
+    });
+
+
+
+
 
 });
 
 //read
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
 
-    Blog.find().then((data)=>{
+    Blog.find().then((data) => {
 
-    res.status(200).send(data);
+        res.status(200).send(data);
 
-      });
+    });
+});
+
+//My Blogs
+app.post('/myBlogs', (req, res) => {
+
+    User.findOne({ username: req.body.username }).then((data, err) => {
+
+        // console.log(req.body)
+        if (data) {
+            res.status(200).send(data);
+        } else {
+            res.send(err);
+        }
+
+    });
 });
 
 // update
-app.post('/update',cpUpload, async(req, res)=>{
+app.put('/update', cpUpload, async (req, res) => {
 
-    const imageBuffer = await req.files['image'][0].buffer;
-    const videoBuffer = await req.files['video'][0].buffer;
+    let imageBuffer = null;
+    let videoBuffer = null;
 
-    Blog.updateOne({_id:req.body.id},{
+    if (req.body.image) {
+        imageBuffer = await req.files['image'][0].buffer;
+
+    }
+
+    if (req.body.video) {
+        videoBuffer = await req.files['video'][0].buffer;
+
+    }
+
+
+
+    Blog.updateOne({ _id: req.body.id }, {
         title: req.body.title,
         content: req.body.content,
         image: imageBuffer,
         video: videoBuffer
-        
-    }).then((data)=>{
+
+    }).then((data) => {
 
         res.status(200).send("Blog Updated");
-      
-      });
+
+    });
 
 
 });
 
+//delete
+app.delete('/delete', (req, res) => {
+
+    Blog.deleteOne({ _id: req.body.id }).then((data) => {
+
+        res.status(200).send(data);
+
+    });
+
+});
+
+//signUp
+app.post('/signUp', async (req, res) => {
+
+    console.log(req.body);
+    User.find({ username: req.body.username }).then((data,err) => {
+
+        if(data){
+
+            res.send("User already exists!");
+        }else{
+            console.log(err);
+        }
+
+    });
+
+
+    const newUser = new User({
+
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    await newUser.save();
+});
+
+//login
+app.post('/login', (req, res) => {
+
+    User.findOne({ username: req.body.username }).then((data, err) => {
+
+        if (data) {
+
+            if (data.password == req.body.password.password) {
+
+                res.status(200).send(data);
+            } else {
+                res.send("Incorrect Password");
+            }
+        } else {
+            console.log(err);
+            res.send("User Not Found!");
+        }
+
+    });
+
+});
 
 app.listen(3000, () => {
     console.log("Server started on port 3000.");
